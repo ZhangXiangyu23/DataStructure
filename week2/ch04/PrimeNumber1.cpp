a92a68e11e40cd656ef3c1dfe21be62537731e7b
ZhangXiangyu <884188766@qq.com>
2021-11-13 20:34:11 +0800

    用定义判断

--- week2/ch04/PrimeNumber1.cpp
+++ week2/ch04/PrimeNumber1.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
+
+//时间复杂度O(N) 
 int main()
 {
 	printf("请输入一个数:\n");
-	int x;
-	scanf("%d",&x);
+	int N;
+	scanf("%d",&N);
 	int count=0;
-	for(int i=2;i<x;i++)
+	for(int i=2;i<N;i++)
 	{
-		if(x%i==0)
+		if(N%i==0)
 		{
 			count++; 
-			printf("%d不是素数!\n",x);
+			printf("%d不是素数!\n",N);
 			break;	
 		}	
 	}
 	
 	if(count==0)
 	{
-		printf("%d是素数!\n",x);	
+		printf("%d是素数!\n",N);	
 	}
 	return 0; 
 		
